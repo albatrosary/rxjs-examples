c92d7e4513b46e5d799619140b1bd4a525ace6ac
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-03-05 16:36:50 +0900

    add events

--- src/index.js
+++ src/index.js
@@ -7,13 +7,34 @@ console.log('Hello World!');
 // DOMイベントの監視
 const emitter = new EventEmitter();
 const button = document.querySelector('button');
-const source = Observable.fromEvent(button, 'click');
-source.subscribe(function (e) {
-  emitter.emit('data', e.clientX + ' ' + e.clientY);
-});
+
+// ボタンクリック
+Observable
+  .fromEvent(button, 'click')
+  .subscribe(function (e) {
+    emitter.emit('click-data', e.clientX + ' ' + e.clientY);
+    emitter.emit('click', 'ほげ');
+  });
+// ボタンへのへのマウスオーバー
+Observable
+  .fromEvent(button, 'mouseover')
+  .subscribe(function (e) {
+    emitter.emit('mouseover-data', e.clientX + ' ' + e.clientY);
+  });
 
 // データイベントの監視
-const sources = Observable.fromEvent(emitter, 'data');
-sources.subscribe(function (x) {
-  console.log('data:', x);
-});
+Observable
+  .fromEvent(emitter, 'click-data')
+  .subscribe(function (x) {
+    console.log('click:', x);
+  });
+Observable
+  .fromEvent(emitter, 'mouseover-data')
+  .subscribe(function (x) {
+    console.log('mouseover:', x);
+  });
+Observable
+  .fromEvent(emitter, 'click')
+  .subscribe(function (x) {
+    console.log('click:', x);
+  });
