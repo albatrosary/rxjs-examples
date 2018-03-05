71f068f766f58df836e9792f69f82e57a180d6f2
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-03-05 17:52:33 +0900

    add comments

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import 'rxjs/add/observable/fromEvent';
 
 console.log('Hello World!');
 
-// DOMイベントの監視
 const emitter = new EventEmitter();
 const button = document.querySelector('button');
 
-// ボタンクリック
+// 1. DOMイベントとイベントエミッターの関連付け
+// 1-1. ボタンクリック
 Observable
   .fromEvent(button, 'click')
   .subscribe(function (e) {
     emitter.emit('click-data', e.clientX + ' ' + e.clientY);
     emitter.emit('click', 'ほげ');
   });
-// ボタンへのへのマウスオーバー
+// 1-2. ボタンへのへのマウスオーバー
 Observable
   .fromEvent(button, 'mouseover')
   .subscribe(function (e) {
     emitter.emit('mouseover-data', e.clientX + ' ' + e.clientY);
   });
 
-// データイベントの監視
+// 2. データイベントの監視
 Observable
   .fromEvent(emitter, 'click-data')
   .subscribe(function (x) {
