2e2c3957af9ecfe1dee14241c4da9bf4b18d2f7c
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-03-05 16:25:06 +0900

    event emitter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+import { Observable } from 'rxjs/Observable';
+import { EventEmitter } from 'events';
+import 'rxjs/add/observable/fromEvent';
 
 console.log('Hello World!');
+
+// DOMイベントの監視
+const emitter = new EventEmitter();
+const button = document.querySelector('button');
+const source = Observable.fromEvent(button, 'click');
+source.subscribe(function (e) {
+  emitter.emit('data', e.clientX + ' ' + e.clientY);
+});
+
+// データイベントの監視
+const sources = Observable.fromEvent(emitter, 'data');
+sources.subscribe(function (x) {
+  console.log('data:', x);
+});
