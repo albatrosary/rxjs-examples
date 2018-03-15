e0795f658ab5c8156662af4226419317da91834a
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-03-15 15:21:33 +0900

    skipWhile

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/throttleTime';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/map';
 
+import 'rxjs/add/observable/range';
+import 'rxjs/add/operator/skipLast';
+import 'rxjs/add/operator/skipWhile';
+
 const button = document.querySelectorAll('button');
 
-let i=0;
 Observable.fromEvent(button[1], 'click')
-  .map(x => (i++)*2)
-  .scan((acc, one) => acc + one, 1)
-  .subscribe(count => console.log(`Clicked ${count} times`));
+  .throttleTime(1000)
+  .map(event => event.clientX)
+  .scan((count, clientX) => count + clientX, 0)
+  .subscribe(count => console.log(count));
+
+Observable.range(1, 5)
+  .skipWhile(x => x < 3)
+  .subscribe(x => console.log(x));
+  
